a752323de77b9b4a6976dbc3ad02041c96817efd
Frank Zcategui <frankuzcategui22@gmail.com>
2025-12-10 16:38:48 -0400

    Agregando comentario a los archvios y creando el chat avanzado (chat_realtime)

--- chat_realtime.py
+++ chat_realtime.py
@@ -8,5 +8,22 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "2356"
 
 #TODO: Inicializar SocketIO con la aplicacion de FLASK
-socketio = SocketIO(app)
+socketio = SocketIO(app) 
 
+@app.route('/')
+
+def index():
+    return render_template('chat_realtime.html', mensajes=[], nombre='Invitado')
+
+@socketio.on('mensaje')
+def manejar_mensaje(data):
+    #TODO Obtener el nombre y el nuevo mensaje del diccionario "data"
+    nombre = data['nombre']
+    nuevo_mensaje = data['mensaje']
+
+# TODO: Emitir un evento actualizar_mensaje a todos los clientes conectados
+    socketio.emit('actualizar_mensajes',{'nombre': nombre,'mensaje': nuevo_mensaje})
+
+#TODO: Ejecutar la aplicacion Flask con el servidor SocketIO en modo depuracion
+if __name__ == '__main__':
+    socketio.run(app,debug=True)
